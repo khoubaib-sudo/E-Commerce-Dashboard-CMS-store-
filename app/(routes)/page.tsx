5d64accff14e47d84ae34acf4ff95985a903471e
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-08-25 15:53:46 +0200

    billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,19 @@
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/ui/billboard";
 export const revalidate = 0;
 
 const HomePage = async () => {
-  return <div className="space-y-10 pb-10">Hello store</div>;
+  
+  const billboard = await getBillboard("69780a28-6ba2-4ac6-8a9e-adcf7af13329");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard}/>    
+      </div>
+    </Container>
+  )
 };
 
 export default HomePage;
